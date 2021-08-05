61cdaf149c2302aa05a52fc5843a88ad9eae7712
pororo0721 <  >
2021-08-05 12:37:47 +0100

    Header Component and Fetch data from the API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+
 // Styles
 import {GlobalStyle} from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
-      Start here.
-      <GlobalStyle/>
+      <Header />
+      <Home />
+      <GlobalStyle />
     </div>
   );
 }
