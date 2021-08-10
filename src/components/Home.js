d1b3997a694db58c1c9784305405ff125cb6dff8
pororo0721 <  >
2021-08-10 14:00:33 +0100

    SearchBar Component -Styles, Logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -18,18 +19,19 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const {state, loading, error } = useHomeFetch();
+    const {state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
     console.log(state);
     return (
         <>
-            {state.results[0] ? (
+            {!searchTerm && state.results[0] ? (
      <HeroImage
      image={`${IMAGE_BASE_URL }${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
      title={state.results[0].original_title}
      text={state.results[0].overview}
      /> 
     ): null}
-    <Grid header='Popular Movies'>
+    <SearchBar setSearchTerm={setSearchTerm}/>
+    <Grid header={ searchTerm? 'Search Result': 'Popular Movies'}>
         {state.results.map(movie=> (
             <Thumb
                 key={movie.id}
@@ -44,6 +46,7 @@ const Home = () => {
         ))}
     </Grid>
     <Spinner/>
+
             
   </>
  ); 
